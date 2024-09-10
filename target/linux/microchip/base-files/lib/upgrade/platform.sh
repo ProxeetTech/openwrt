9caa5d943f7f2f150fb558bfbd8f724a568b2b67
cjack <cjack@yandex.ru>
2024-09-10 22:19:53 +0300

    platform_do_upgrade fix

--- target/linux/microchip/base-files/lib/upgrade/platform.sh
+++ target/linux/microchip/base-files/lib/upgrade/platform.sh
@@ -4,61 +4,44 @@ REQUIRE_IMAGE_METADATA=1
 RAMFS_COPY_BIN='fw_printenv fw_setenv'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
-platform_check_image() {
-	local diskdev partdev diff
+PROXEET_ACTIVE_PART=5
 
+platform_check_image() {
 	[ "$#" -gt 1 ] && return 1
 
-	export_bootdevice && export_partdevice diskdev 0 || {
-		echo "Unable to determine upgrade device"
-		return 1
-	}
-
 	echo "platform_check_image"
-
 	return 0;
 }
 
 platform_do_upgrade() {
-	local diskdev partdev diff
+	local diskdev boot_part
 
-	export_bootdevice && export_partdevice diskdev 0 || {
-		echo "Unable to determine upgrade device"
-		return 1
-	}
-
+	echo "platform_do_upgrade"
 	boot_part=`fw_printenv -n mmc_cur`
-	active_part=5
-	[ "$boot_part" -eq "5" ] && active_part=6
+	[ "$boot_part" -eq "5" ] && PROXEET_ACTIVE_PART=6
 	echo "Boot part: ""$boot_part"
-	echo "Try to write part: ""$active_part"" with file: ""$1"
-	diskdev="/dev/mmcblk0p""$active_part"
+	echo "Try to write part: ""$PROXEET_ACTIVE_PART"" with file: ""$1"
+	diskdev="/dev/mmcblk0p""$PROXEET_ACTIVE_PART"
 	echo "diskdev: ""$diskdev"
 
 	get_image_dd "$1" of="$diskdev" bs=4096
-
 	sync
 
-	echo "platform_do_upgrade"
+	# change boot partition
+	fw_setenv mmc_cur "$PROXEET_ACTIVE_PART"
+	sync
+
+	echo "platform_do_upgrade exit"
 
 	return 0;
 }
 
 platform_copy_config() {
-	local partdev
-
-	if export_partdevice partdev 1; then
-		mkdir -p /boot
-	fi
+	local diskdev
 
 	echo "platform_copy_config"
 
-	boot_part=`fw_printenv -n mmc_cur`
-	active_part=5
-	[ "$boot_part" -eq "5" ] && active_part=6
-	echo "Boot part: ""$boot_part"
-	echo "Try to write part: ""$active_part"" with file: ""$1"
-	diskdev="/dev/mmcblk0p""$active_part"
+	diskdev="/dev/mmcblk0p""$PROXEET_ACTIVE_PART"
 	echo "diskdev: ""$diskdev"
 
 	mkdir -p /tmp/recovery
@@ -67,9 +50,7 @@ platform_copy_config() {
 	sync
 	umount /tmp/recovery
 
-	# change boot partition
-	fw_setenv mmc_cur "$active_part"
-	sync
+	echo "platform_copy_config exit"
 
 	return 0;
 }
